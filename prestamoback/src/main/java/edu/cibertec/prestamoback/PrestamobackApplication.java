a0aa58e698b261794b322ac28a25c6349be11e23
ernesto.bazan <ernesto.bazan@bbva.com>
2023-01-23 23:40:34 -0500

    first commit

---
+++ prestamoback/src/main/java/edu/cibertec/prestamoback/PrestamobackApplication.java
@@ -0,0 +1,13 @@
+package edu.cibertec.prestamoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestamobackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestamobackApplication.class, args);
+	}
+
+}
---
+++ prestamoback/src/test/java/edu/cibertec/prestamoback/PrestamobackApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cibertec.prestamoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestamobackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
